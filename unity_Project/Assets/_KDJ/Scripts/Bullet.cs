c3531c8f65de8be4fb272080c0629ca14d37fea4
김동주 <ehdwn7531@gmail.com>
2020-06-04 12:26:44 +0900

    -보스생성
    -총알패턴1 : 플레이어를 향해서 총알발사
    -총알패턴2 : 총알 최대갯수에 맞춰 회전 총알 발사

--- unity_Project/Assets/_KDJ/Scripts/Bullet.cs
+++ unity_Project/Assets/_KDJ/Scripts/Bullet.cs
@@ -7,18 +7,31 @@ public class Bullet : MonoBehaviour
 {
     public float speed = 10.0f;
 
+    GameObject player;
+    GameObject boss;
+
     // Start is called before the first frame update
     void Start()
     {
-
+       player = GetComponent<GameObject>();
+       boss = GetComponent<GameObject>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        ////if (boss.transform.CompareTag("BOSS"))
+        ////    transform.Translate(-transform.up* speed * Time.deltaTime);
+        //if(player.transform.CompareTag("PLAYER")) transform.Translate(Vector3.up * speed * Time.deltaTime);
+        ////
+        //if (boss.transform.CompareTag("BOSS")) transform.Translate(Vector3.down * speed * Time.deltaTime);
         transform.Translate(Vector3.up * speed * Time.deltaTime);
     }
 
+    //IEnumerator BossBullet()
+    //{
+    //
+    //}
     //private void OnBecameInvisible()
     //{
     //    Destroy(gameObject);
