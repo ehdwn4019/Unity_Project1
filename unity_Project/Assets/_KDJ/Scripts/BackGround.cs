911a9135f0cf133a4657f94e04d56935f239f505
김동주 <ehdwn7531@gmail.com>
2020-06-08 13:57:40 +0900

    -알파버전 모델교체 -백그라운드 스크롤링 -캐릭터 모델교체(플레이어,에너미,보스)

---
+++ unity_Project/Assets/_KDJ/Scripts/BackGround.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGround : MonoBehaviour
+{
+    Material mat;
+    public float scrollSpeed = 0.1f;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        mat=GetComponent<Renderer>().material;
+
+       //mat = GetComponent<Material>();
+       //offset = new Vector2(0, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //offset = offset * scrollSpeed * Time.deltaTime;
+        //
+        //BackGroundScroll();
+        //
+        //mat.GetTextureOffset("bg");
+        //mat.SetTextureOffset("bg", offset);
+        BackGroundScroll();
+    }
+
+    private void BackGroundScroll()
+    {
+        Vector2 offset = mat.mainTextureOffset;
+        offset.Set(0, offset.y+(scrollSpeed*Time.deltaTime));
+        mat.mainTextureOffset = offset;
+    }
+}
